8924223aec630896121ec53a8545306a4c28fe45
Douglas Rodrigues <douglas.fraga@gmail.com>
2022-10-31 15:48:04 -0300

    initial commit

---
+++ translate_sheet.py
@@ -0,0 +1,19 @@
+from googletrans import Translator
+import pandas as pd
+
+
+f = pd.read_excel('sheet.xlsx')
+
+
+cols = []
+for col in ['Key Words', 'Study Area', 'Available data', 'Main Results']: # Columns to translate
+    print(col)
+    lines = []
+    for line in f[col]:
+        trans = Translator()
+        trans_text = trans.translate(line, src="pt", dest="en")
+        lines.append(trans_text.text)
+    f[col] = lines
+
+f.to_excel("output2.xlsx", index=False)
+
